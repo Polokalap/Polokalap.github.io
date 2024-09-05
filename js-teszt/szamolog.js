81911359641142a05d4c269db2536dc356e80b6c
Polokalap <116758671+Polokalap@users.noreply.github.com>
2024-09-05 22:22:19 +0200

    hehe

---
+++ js-teszt/szamolog.js
@@ -0,0 +1,38 @@
+let preview = 0;
+
+function egy() {
+    preview += 1;
+    document.getElementById('eredmeny').innerHTML = preview;
+}
+function ketto() {
+    preview += 2;
+    document.getElementById('eredmeny').innerHTML = preview;
+}
+function harom() {
+    preview += 3;
+    document.getElementById('eredmeny').innerHTML = preview;
+}
+function negy() {
+    preview += 4;
+    document.getElementById('eredmeny').innerHTML = preview;
+}
+function ot() {
+    preview += 5;
+    document.getElementById('eredmeny').innerHTML = preview;
+}
+function hat() {
+    preview += 6;
+    document.getElementById('eredmeny').innerHTML = preview;
+}
+function het() {
+    preview += 7;
+    document.getElementById('eredmeny').innerHTML = preview;
+}
+function nyolc() {
+    preview += 8;
+    document.getElementById('eredmeny').innerHTML = preview;
+}
+function kilenc() {
+    preview += 9;
+    document.getElementById('eredmeny').innerHTML = preview;
+}

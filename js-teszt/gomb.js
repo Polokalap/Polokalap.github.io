411b0bf2310a8d9d0e5aec914dc2e5aa838f9923
Polokalap <116758671+Polokalap@users.noreply.github.com>
2024-09-05 18:15:43 +0200

    utálom a jst

---
+++ js-teszt/gomb.js
@@ -0,0 +1,6 @@
+let gombint = 0;
+
+function gomb() {
+    gombint += 1;
+    document.getElementById('gombbutton').innerHTML = gombint;
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+function meow() {
+    document.getElementById('meow').innerHTML = "🧶 meow"
+
+    document.getElementById('js-teszt').innerHTML = "🤖 js teszt"
+
+    document.getElementById('js').innerHTML = "js:"
+}
